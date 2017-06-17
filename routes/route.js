d6c8b50224344164bca8f494244ff5f0865b7f87
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-06-17 17:50:50 +0530

    Completed

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,7 @@ var express = require('express');
 var multer  = require('multer');
 var upload = multer({dest: __dirname + '../upload'});
 var path = require('path');
+var fs = require('fs');
 
 var router = express.Router();
 
@@ -15,6 +16,8 @@ router.get('/', function (req, res) {
 var fileSize = {};
 router.post('/upload', upload.single('file'), function (req, res) {
     fileSize.size = req.file.size;
+    console.log(req.file);
+    fs.unlinkSync(path.join(__dirname + '../upload/' + req.file.filename));
     res.set({'Content-Type': 'application/json'});
     res.send(JSON.stringify(fileSize));
 });

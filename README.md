360e44892343099a173f84c042283b7c36e7c50a
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2017-06-17 17:46:45 +0530

    Completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FileMetadata
 File Metadata service using nodejs
+
+# Preview
+<a href="https://filemdservice.herokuapp.com">File Metadata</a>
---
+++ routes/route.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var express = require('express');
+var multer  = require('multer');
+var upload = multer({dest: __dirname + '../upload'});
+var path = require('path');
+
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    res.set({'Content-Type': 'text/html'});
+    res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+var fileSize = {};
+router.post('/upload', upload.single('file'), function (req, res) {
+    fileSize.size = req.file.size;
+    res.set({'Content-Type': 'application/json'});
+    res.send(JSON.stringify(fileSize));
+});
+
+module.exports = router;
